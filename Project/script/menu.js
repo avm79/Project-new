94a38508f893db094de8245e563d52aba15c3ecf
avm79 <116026226+avm79@users.noreply.github.com>
2022-12-09 00:08:56 +0200

    Add files via upload

--- Project/script/menu.js
+++ Project/script/menu.js
@@ -7,7 +7,7 @@ $('#menuToggle #toggleCheckbox').change(function() {
     }
 });
 
-$('#home').on('click', function(e){
+$('#about-link').on('click', function(e){
     e.preventDefault()
     $('html, body').animate({scrollTop: $('#about').offset().top}, 500)
    document.getElementById('toggleCheckbox').checked = false
--- Project/script/script.js
+++ Project/script/script.js
@@ -5,16 +5,13 @@ $(this).find('.about_pritatle').toggleClass('about_pritatle-color');
 $(this).find('.about-town').toggleClass('about-town-color');
 $(this).find('.about-icon-block').toggleClass('about-icon-block-color');
 $(this).next('.about-text').slideToggle(300);
-
 })
 
 $('.customers_items>div').on('click',function(){
     $(this).find('.customers_subtitle_text').toggleClass('customers_subtitle_tex-color');
     $(this).next('.customers_text').slideToggle(300);
-    
 })
 
 $('.customers_items').on('click',function(){
     $(this).find('.customers_line').toggleClass('customers_line-color');
-       
 })
